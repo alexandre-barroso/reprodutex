4a34808b91d3ba3b599eb3dedaec39c0f2a4db25
Alexandre Barroso <42815351+alexandre-barroso@users.noreply.github.com>
2020-12-15 17:27:38 -0300

    Add files via upload

---
+++ reprodutex.py
@@ -0,0 +1,78 @@
+# Rede Produtora de Texto = ReProduTex
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+from keras.models import load_model
+import numpy as np
+import random
+import sys
+
+np.seterr(divide = 'ignore') 
+
+rede_neural = load_model('rede_neural.h5')
+
+with open('corpus_completo.txt', 'r') as file:
+    corpus = file.read().lower()
+    
+caracteres = sorted(list(set(corpus)))
+limite = 40
+car_indices = dict((c, i) for i, c in enumerate(caracteres))
+indices_car = dict((i, c) for i, c in enumerate(caracteres))
+
+def amostra(preds, liberdade=1.0):
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds) / liberdade
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
+    
+def producao_textual(tamanho, liberdade):
+    inicio = random.randint(0, len(corpus) - limite - 1)
+    gerado = ''
+    frase = corpus[inicio: inicio + limite]
+    gerado += frase
+    for i in range(tamanho):
+            x_pred = np.zeros((1, limite, len(caracteres)))
+            for t, caractere in enumerate(frase):
+                x_pred[0, t, car_indices[caractere]] = 1.
+
+            preds = rede_neural.predict(x_pred, verbose=0)[0]
+            prox_indice = amostra(preds, liberdade)
+            proximo_caractere = indices_car[prox_indice]
+
+            gerado += proximo_caractere
+            frase = frase[1:] + proximo_caractere
+ 
+            sys.stdout.write(proximo_caractere)
+            sys.stdout.flush()
+            
+    print()
+
+print('\n>>> Rede Produtora de Texto (ReProduTex)\n')
+
+print(">>> Produzindo...\n")
+
+
+print('-> (liberdade: 0.2)\n')
+producao_textual(500, 0.4)
+print('\n')
+
+print('-> (liberdade: 0.3)\n')
+producao_textual(500, 0.4)
+print('\n')
+
+print('-> (liberdade: 0.4)\n')
+producao_textual(500, 0.4)
+print('\n')
+
+print('-> (liberdade: 0.5)\n')
+producao_textual(500, 0.5)
+print('\n')
+
+print('-> (liberdade: 0.6)\n')
+producao_textual(500, 0.6)
+print('\n')
+
+
+print('\n-> Encerrando programa...\n')
---
+++ treinar.py
@@ -0,0 +1,150 @@
+#deleta conteúdo do corpus antigo
+corpus_zerado = open('corpus_completo.txt', 'r+')
+corpus_zerado.truncate(0)
+corpus_zerado.close()
+
+#abre corpus parcial 1
+with open('corpus_1.txt', 'r') as file :
+  corpus_1 = file.read()
+
+#abre corpus parcial 2
+with open('corpus_2.txt', 'r') as file:
+  corpus_2 = file.read()
+  
+  #transforma em string
+  corpus_1_txt = str(corpus_1)
+  corpus_2_txt = str(corpus_2)
+  
+  #soma ambos corpora
+  corpus_completo = corpus_1_txt + corpus_2_txt
+
+#escreve as strings no arquivo vazio
+with open('corpus_completo.txt', 'w') as file:
+  file.write(corpus_completo)
+  
+#abre corpus
+with open('corpus_completo.txt', 'r') as file :
+  corpus = file.read()
+
+#mudança no corpus
+### INSERIR AQUI TODAS AS MUDANÇAS
+#corpus = corpus.replace("e'", 'é')
+
+#reescreve corpus
+with open('corpus_completo.txt', 'w') as file:
+  file.write(corpus)
+  
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+
+import numpy as np
+import random
+import sys
+
+from keras.models import load_model
+from keras.callbacks import *
+from keras.callbacks import ReduceLROnPlateau
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from keras.layers import LSTM
+from keras.optimizers import RMSprop
+
+print('\n>>> Inicializando corpus...\n')
+
+#Manipulação textual:
+
+with open('corpus_completo.txt', 'r') as file:
+    corpus = file.read().lower()
+
+print('Tamanho das amostras:', len(corpus))
+
+caracteres = sorted(list(set(corpus))) #coletando todos os caracteres únicos
+
+print('Quantidade de caracteres únicos: ',len(caracteres))
+
+car_indices = dict((c, i) for i, c in enumerate(caracteres))
+
+indices_car = dict((i, c) for i, c in enumerate(caracteres))
+
+limite = 40
+
+passos = 3
+
+frases = []
+
+proximos_caracteres = []
+
+for i in range(0, len(corpus) - limite, passos):
+    frases.append(corpus[i: i + limite])
+    proximos_caracteres.append(corpus[i + limite])
+    
+x = np.zeros((len(frases), limite, len(caracteres)), dtype=np.bool)
+
+y = np.zeros((len(frases), len(caracteres)), dtype=np.bool)
+
+for i, frase in enumerate(frases):
+    for t, caractere in enumerate(frase):
+        x[i, t, car_indices[caractere]] = 1
+    y[i, car_indices[proximos_caracteres[i]]] = 1
+
+### FASE DE TREINO
+
+print('\n>>> Inicializando rede neural...\n')
+  
+#Rede neural:
+#1. Camada LSTM
+#2. Camada densa totalmente conectada
+#3. Função de ativação softmax
+#RMSprop + Categorical Crossentropy
+
+rede_neural = Sequential()
+rede_neural.add(LSTM(128, input_shape=(limite, len(caracteres))))
+rede_neural.add(Dense(len(caracteres)))
+rede_neural.add(Activation('softmax'))
+    
+print('>>> DIGITE: \n\n 1. "c" para continuar treino de onde parou. \n\n 2. "r" para re-treinar do começo. \n\n 3. qualquer outra coisa para encerrar o programa. \n\n OBSERVAÇÃO: digite "c" ou "r" sem aspas!\n')
+
+opcao = input('> ')
+
+epoch = input('\n> Quantas iterações (epochs) a rede neural deve rodar?\n>  ')
+epoch = int(epoch)
+
+batch_size = input('\n> Qual o batch_size?\n>  ')
+batch_size = int(batch_size)
+print('')
+
+if opcao.lower() == 'r':
+
+    otimizador = RMSprop(lr=0.01)
+    rede_neural.compile(loss='categorical_crossentropy', optimizer=otimizador)
+    peso_salvos = "neuronios.hdf5"
+    checkpoint = ModelCheckpoint(peso_salvos, monitor='loss',
+                                 verbose=1, save_best_only=True,
+                                 mode='min')
+    redutor = ReduceLROnPlateau(monitor='loss', factor=0.2,
+                                  patience=1, min_lr=0.001)
+    callbacks = [checkpoint, redutor]
+    rede_neural.fit(x, y, batch_size=batch_size, epochs=epoch, callbacks=callbacks)
+    rede_neural.save('rede_neural.h5')
+
+    print('\n>>> Finalizando treino da rede neural...\n')
+    
+elif opcao.lower() == 'c':
+
+    del rede_neural
+    rede_neural = load_model('rede_neural.h5')
+    peso_salvos = "neuronios.hdf5"
+    checkpoint = ModelCheckpoint(peso_salvos, monitor='loss',
+                                 verbose=1, save_best_only=True,
+                                 mode='min')
+    redutor = ReduceLROnPlateau(monitor='loss', factor=0.2,
+                                  patience=1, min_lr=0.001)
+    callbacks = [checkpoint, redutor]
+    rede_neural.fit(x, y, batch_size=batch_size, epochs=epoch, callbacks=callbacks)
+    rede_neural.save('rede_neural.h5')
+
+    print('\n>>> Finalizando treino da rede neural...\n')
+
+else:
+
+   print('\n>>> Encerrando programa...\n')    
